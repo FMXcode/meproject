542c98c0897c9b4a4bc2b239f087559e13dda881
payan <payan@DESKTOP-27MPM83>
2021-10-23 18:27:25 +0300

    Добавьте файлы проекта.

---
+++ meproject/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace meproject
+{
+    class group
+    {
+        public
+        int name;
+        int yeur;
+        int noumber;
+
+
+        vid GetX(string  name, int  yeur, int  noumber)
+        {
+            ref name = name;
+            ref yeur = yeur;
+            ref noumber = noumber;
+
+        }
+        
+       int SetX()
+        {
+            return name;
+            return yeur;
+            return noumber;
+        }
+
+
+
+    }
+    class Program
+    { 
+        static void Main(string[] args)
+        { 
+            
+            group A = new group();
+            GetX(A);
+            SetX(a);
+
+        }
+    }
+}
